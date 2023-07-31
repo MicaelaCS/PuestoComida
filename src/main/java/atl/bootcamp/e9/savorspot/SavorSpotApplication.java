fa328ce93a0073cab45f24148d736a9dc85b3125
alejandro <cesar.alejandro.fernandez.7@gmail.com>
2023-07-31 19:14:33 -0300

    initial commit

---
+++ src/main/java/atl/bootcamp/e9/savorspot/SavorSpotApplication.java
@@ -0,0 +1,13 @@
+package atl.bootcamp.e9.savorspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavorSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavorSpotApplication.class, args);
+	}
+
+}
