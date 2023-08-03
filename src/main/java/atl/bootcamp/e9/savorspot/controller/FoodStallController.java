b31d40d1b3e7fdf8819b8e82b210f7db7e323339
LuisZG <lazg12@gmail.com>
2023-08-03 16:15:51 -0500

    probando si hago push en mi rama,disculpen

--- src/main/java/atl/bootcamp/e9/savorspot/controller/FoodStallController.java
+++ src/main/java/atl/bootcamp/e9/savorspot/controller/FoodStallController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/stalls")
+@RequestMapping("/stalls") // Probando mi rama Lucho
 public class FoodStallController {
 
     private final RegisterFoodStallService registerFoodStallService;
