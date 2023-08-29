a4fb66785b301ffa3df449c239996a8a3a57121b
ravenfahs <raven_fahs@hotmail.com>
2023-08-29 15:31:48 -0500

    actualizacion

--- src/main/java/atl/bootcamp/e9/savorpot/service/GetOrderByIdService.java
+++
@@ -1,7 +0,0 @@
-package atl.bootcamp.e9.savorpot.service;
-
-import atl.bootcamp.e9.savorpot.DTO.OrderDTO;
-
-public interface GetOrderByIdService {
-    OrderDTO getOrderbyId(Long id, Long userID);
-}
--- src/main/java/atl/bootcamp/e9/savorpot/service/UpdateOrderStatusService.java
+++
@@ -1,8 +0,0 @@
-package atl.bootcamp.e9.savorpot.service;
-
-import org.springframework.http.ResponseEntity;
-
-public interface UpdateOrderStatusService {
-
-    ResponseEntity<String> updateOrder(Long userID, Long id);
-}
